f65701a5d210e5484f19e595e54e0ba411656275
Server Kumar <ubuntu@mishanenergy.com>
2014-12-09 12:31:55 +0000

    Fixes with form processor

--- form.php
+++ form.php
@@ -2,17 +2,18 @@
  
 $email_to = "shamail@traceback.in";
 $email_subject = "[traceback.in] Contact Form Posted";
+$email_from = "noreply@traceback.in";
 
-$first_name =  isset ($_POST['name']) ? $_POST['name'] : "Unspecified";
-$email_from =  isset ($_POST['email']) ? $_POST['email'] : "Unspecified";
-$subject =  isset ($_POST['subject']) ? $_POST['subject'] : "Unspecified";
-$message =  isset ($_POST['message']) ? $_POST['message'] : "Unspecified";
+$first_name =  isset ($_POST['contactName']) ? $_POST['contactName'] : "Unspecified";
+$email =  isset ($_POST['contactEmail']) ? $_POST['contactEmail'] : "Unspecified";
+$subject =  isset ($_POST['contactSubject']) ? $_POST['contactSubject'] : "Unspecified";
+$message =  isset ($_POST['contactMessage']) ? $_POST['contactMessage'] : "Unspecified";
 
 $email_message = "Form details below.\n\n";
-$email_message .= "First Name: ".$first_name."\n";
-$email_message .= "Email: ".$email_from."\n";
-$email_message .= "Subject: ".$subject."\n";
-$message .= "Comments: ".$message."\n";
+$email_message .= "First Name - ".$first_name."\n";
+$email_message .= "Email - ".$email."\n";
+$email_message .= "Phone - ".$subject."\n\n";
+$email_message .= $message."\n";
 
 $headers = 'From: '.$email_from."\r\n".  'Reply-To: '.$email_from."\r\n" .  'X-Mailer: PHP/' . phpversion();
 @mail($email_to, $email_subject, $email_message, $headers);  
